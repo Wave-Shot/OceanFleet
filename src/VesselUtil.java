19cd5e010264d683ae7d585036a2dba568a69d22
Wave-Shot <vsh.2509@gmail.com>
2026-02-03 15:35:56 +0530

    [Srihari]: UC3 implement getVesselById with linear search

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -6,4 +6,13 @@ public class VesselUtil {
     public void addVesselPerformance(Vessel vessel) {
         vesselList.add(vessel);
     }
+    public Vessel getVesselById(String vesselId) {
+        for (int i = 0; i < vesselList.size(); i++) {
+            if (vesselList.get(i).getVesselId().equals(vesselId)) {
+                return vesselList.get(i);
+            }
+        }
+        return null;
+    }
+
 }
